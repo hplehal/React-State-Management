7475f9a1539531dcce778c37970ccd8de0247c2c
Tej Lehal <tejlehal13@gmail.com>
2020-08-12 22:36:48 -0400

    Update Readme

--- src/App.js
+++ src/App.js
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>State Management</h1>
+      <p>This repository will help solidify the different ways of manipulating States.</p>
     </div>
   );
 }
