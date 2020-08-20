8ad776ea6b2c8ad1d8350acbd802c7ec2e821aee
Tej Lehal <tejlehal13@gmail.com>
2020-08-19 23:01:00 -0400

    Added Class State based Increment, decrement and reset

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Class Base State Management!</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      state: 0
+    }
+  }
+
+  increment = () => {
+    this.setState({
+      state: this.state.state + 1
+    })
+
+    // this.setState(({ state }) => {
+    //   return { state: state + 1 };
+    // }));
+
+  }
+
+  decrement = () => {
+    this.setState({
+      state: this.state.state - 1
+    })
+  }
+
+  reset = () => {
+    this.setState({
+      state: 0
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.state}</h1>
+        <button onClick={() => this.increment()} >Increment</button>
+        <button onClick={() => this.decrement()}>Decrement</button>
+        <button onClick={() => this.reset()}>Reset</button>
+      </div>
+    )
+  }
 }
 
 export default App;
+
